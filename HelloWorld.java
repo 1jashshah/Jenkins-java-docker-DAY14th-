d2fef7c8d6bd0f1a66e55f985b14279f6e2f5574
Jash Shah <132209796+1jashshah@users.noreply.github.com>
2024-07-31 16:29:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
+package com.example;
+
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Day - Hello Task!");
     }
 }
